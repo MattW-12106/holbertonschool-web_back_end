2a12de5299a8710b712e7987c8fe163a8b3d2738
MattW-12106 <12106@holbertonstudents.com>
2026-03-29 21:01:16 +1100

    defines a function to_str that takes a float and returns string representation of the float

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+defines a function to_str that takes a float
+and returns string representation of the float
+"""
+
+
+def to_str(n: float) -> str:
+    """Return the string representation of n"""
+    return str(n)
