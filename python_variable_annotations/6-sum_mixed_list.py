fe5af7de5bbe90a78fdd74d63f16186dc7beb213
MattW-12106 <12106@holbertonstudents.com>
2026-03-29 21:47:07 +1100

    pycodestyle changes

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -4,9 +4,8 @@ defines function sum_mixed_list
 which takes a list mxd_lst of integers
 and floats and returns their sum as a float
 """
-
-
 import typing
+
 
 def sum_mixed_list(mxd_lst: typing.List[typing.Union[int, float]]) -> float:
     """Return the sum of all the integers and floats in mxd_lst as a float"""
