55444d16a9002a3a0da2b742a46814d72d17093c
MattW-12106 <12106@holbertonstudents.com>
2026-03-29 21:58:41 +1100

    function make_multiplier that takes a float multiplier as argument and returns a function that multiplies a float by multiplier

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -5,8 +5,8 @@ that takes a float multiplier as argument
 and returns a function that multiplies a float by multiplier
 """
 
-
 import typing
+
 
 def make_multiplier(multiplier: float) -> typing.Callable[[float], float]:
     """Return a function that multiplies a float by multiplier"""
