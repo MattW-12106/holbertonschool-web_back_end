7c2a28ef04420127a4fa98e59194b3d890ac0bb2
MattW-12106 <12106@holbertonstudents.com>
2026-03-29 21:52:17 +1100

    curse typinggit add .!

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -5,8 +5,9 @@ float v as arguments and returns a tuple.
 The first element of the tuple is the string k.
 The second element is the square of the int/float v and should be annotated as a float
 """
+import typing
 
 
-def to_kv(k: str, v: int | float) -> tuple[str, float]:
+def to_kv(k: str, v: typing.Union[int, float]) -> typing.Tuple[str, float]:
     """Return a tuple of k and the square of v as a float"""
     return (k, float(v ** 2))
