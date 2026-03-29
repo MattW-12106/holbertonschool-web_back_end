127df966c08d90aba0b0b6de423953743677e6bf
MattW-12106 <12106@holbertonstudents.com>
2026-03-29 20:56:23 +1100

    pycodestyle changes

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-
-
 """defines a function that adds 2 numbers and returns a float"""
+
+
 def add(a: float, b: float) -> float:
     """Return the sum of a and b as a float"""
     return a + b
