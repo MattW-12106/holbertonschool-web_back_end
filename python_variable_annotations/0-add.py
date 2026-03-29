23ba17c8a798789eeafc2a03858cb4606eab488f
MattW-12106 <12106@holbertonstudents.com>
2026-03-29 20:45:34 +1100

    add function returning the sum of two numbers as a float

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""defines a function that adds 2 numbers and returns a float"""
 def add(a: float, b: float) -> float:
     """Return the sum of a and b as a float"""
     return a + b
