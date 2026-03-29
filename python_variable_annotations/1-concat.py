c65436be1a8c0f43811d5bb1aa34ac7d2e8c277a
MattW-12106 <12106@holbertonstudents.com>
2026-03-29 20:50:21 +1100

    function that concats two strings!

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""defines a function that concatenates 2 strings and returns a concatenated string"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Return the concatenation of str1 and str2 as a string"""
+    return str1 + str2
