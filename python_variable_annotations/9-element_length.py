678e1cb9f56cfdd2cba6cc33149c2684d714653a
MattW-12106 <12106@holbertonstudents.com>
2026-03-29 22:04:08 +1100

    9. Let's duck type an iterable object

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -6,6 +6,7 @@ and return values with the appropriate types
 import typing
 
 
-def element_length(lst: typing.List[str]) -> typing.List[typing.Tuple[str, int]]:
+def element_length(lst: typing.Iterable[typing.Sequence]
+) -> typing.List[typing.Tuple[typing.Sequence, int]]:
     """Return a list of tuples, where each tuple contains a string from lst"""
     return [(i, len(i)) for i in lst]
