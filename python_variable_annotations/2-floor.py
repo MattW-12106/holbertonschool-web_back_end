d1356cb85c6ed4d2920d728da0dbc028beabbde7
MattW-12106 <12106@holbertonstudents.com>
2026-03-29 20:55:32 +1100

    defines a floor function which takes a float and returns floor of float

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""defines a floor function which takes a float and returns floor of float"""
+
+
+def floor(n: float) -> int:
+    """returns the floor of flaot n"""
+    return int(n)
