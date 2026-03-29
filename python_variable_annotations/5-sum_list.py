216fd9383a90c1c4d023d3cf5d1995ecbc802779
MattW-12106 <12106@holbertonstudents.com>
2026-03-29 21:36:13 +1100

    function sum_list which takes a list input_list of floats as argument and returns their sum as a float

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+defines a function sum_list which takes
+which takes a list input_list of floats and
+returns their sum as a float.
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """Return the sum of all the floats in input_list"""
+    return sum(input_list)
