213b64dc01520fb38cc697168164b35cc47c0585
MattW-12106 <12106@holbertonstudents.com>
2026-04-02 13:16:05 +1100

    coroutine called async_generator that takes no arguments

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""coroutine called async_generator that takes no arguments"""
+# imports
+import asyncio
+import random
+
+
+async def async_generator():
+    """Yield a random number between 0 and 10 every second, 10 times."""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
