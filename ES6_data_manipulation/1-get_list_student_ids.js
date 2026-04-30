4cb36c45e99c02b8d22e922eee9aa5fa026cb199
MattW-12106 <12106@holbertonstudents.com>
2026-04-30 20:14:45 +1000

    returns empt array THEN maps array

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,5 +1,7 @@
-import getListStudents from './0-get_list_students';
+export default function getListStudentIds(students) {
+  if (!Array.isArray(students)) {
+    return [];
+  }
 
-export default function getListStudentIds() {
-    return getListStudents().map((student) => student.id);
+  return students.map((student) => student.id);
 }
