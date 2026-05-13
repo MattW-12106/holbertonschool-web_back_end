2f7864d0f5ee0523a827084649af55c044f62a09
MattW-12106 <12106@holbertonstudents.com>
2026-05-13 21:21:21 +1000

    reading database and counting the students within it

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+ 
+const countStudents = (path) => {
+  let data;
+  try {
+    data = fs.readFileSync(path, 'utf8');
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+ 
+  const lines = data.split('\n').filter((line) => line.trim() !== '');
+  const students = lines.slice(1);
+ 
+  console.log(`Number of students: ${students.length}`);
+ 
+  const fields = {};
+  for (const student of students) {
+    const columns = student.split(',');
+    const firstname = columns[0];
+    const field = columns[3];
+ 
+    if (!fields[field]) {
+      fields[field] = [];
+    }
+    fields[field].push(firstname);
+  }
+ 
+  for (const [field, names] of Object.entries(fields)) {
+    console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
+  }
+};
+ 
+module.exports = countStudents;
